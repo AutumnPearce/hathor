462c72ba1d6255a241548060587b594e8722d7b5
Autumn Pearce <69016173+AutumnPearce@users.noreply.github.com>
2025-12-01 16:55:43 -0600

    create __init__.py

---
+++ src/hathor/__init__.py
@@ -0,0 +1,7 @@
+"""hathor: Multi-agent system for generating galaxy formation hypotheses and creating relevant plots from RAMSES simulation data."""
+__version__ = "0.0.0"
+__author__ = "Autumn Pearce, Yevhen Kylivnyk"
+
+
+from .hathor import Hathor
+__all__ = ["Hathor"]
